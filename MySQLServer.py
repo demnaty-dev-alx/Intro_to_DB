e885ea0f19924f4aed486090825a10087e1d0a80
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-01-28 00:21:38 +0100

    fix MySQLServer.py script

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,18 +1,24 @@
 import mysql.connector
+from mysql.connector import Error
 
-connection = mysql.connector.connect(
-    user = 'alx_student',
-    password = 'empty@password',
-    host = '127.0.0.1',
-    use_pure = True
-)
-
-cursor = connection.cursor()
 try:
-    cursor.execute("CREATE DATABASE alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        user='alx_student',
+        password='empty@password',
+        use_pure=True
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error: {e}")
+
 finally:
-    cursor.close()
-    connection.close()
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed.")
